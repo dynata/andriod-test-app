bb16fa6708fd1e77fe988bdb9793c9539e4d531f
rick.byington <rick.byington@dynata.com>
2020-07-09 07:53:34 -0400

    init commit

---
+++ app/src/main/java/com/dynata/testapplication/FirstFragment.kt
@@ -0,0 +1,34 @@
+package com.dynata.testapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        view.findViewById<WebView>(R.id.webview).settings.javaScriptEnabled = true
+        view.findViewById<WebView>(R.id.webview).loadUrl("https://www.opinionrewardspanel.com/en/signup?id=o7vlna4lqtpligb")
+    }
+}
